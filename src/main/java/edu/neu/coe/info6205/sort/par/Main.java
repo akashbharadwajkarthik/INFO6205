582575e548a0a0492c0df54840ed0e7f74a39a70
Akash Bharadwaj Karthik <bharadwajkarthik.a@northeastern.edu>
2023-02-18 02:38:28 -0500

    Assignment-5 submission

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -18,43 +18,50 @@ public class Main {
 
     public static void main(String[] args) {
         processArgs(args);
-        System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
-        Random random = new Random();
-        int[] array = new int[2000000];
-        ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
-            // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-            long time;
-            long startTime = System.currentTimeMillis();
-            for (int t = 0; t < 10; t++) {
-                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-                ParSort.sort(array, 0, array.length);
+        for(int size = 100000; size <= 400000; size += 100000) {
+            System.out.println("Size of Array: "+size);
+            for(int thread = 2; thread <= 64; thread += thread){
+                ForkJoinPool pool = new ForkJoinPool(thread);
+                System.out.println("Degree of parallelism: " + pool.getParallelism());
+                Random random = new Random();
+                int[] array = new int[size];
+                ArrayList<Long> timeList = new ArrayList<>();
+                for (int j = 0; j < 10; j++) {
+                    ParSort.cutoff = (size/10) * (j + 1);
+                    // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+                    long time;
+                    long startTime = System.currentTimeMillis();
+                    for (int t = 0; t < 10; t++) {
+                        for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+                        ParSort.sort(array, 0, array.length,pool);
+                    }
+                    long endTime = System.currentTimeMillis();
+                    time = (endTime - startTime);
+                    timeList.add(time);
+
+
+                    System.out.println("cutoff: " + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
+
+                }
+                try {
+                    FileOutputStream fis = new FileOutputStream("./Assignments/Assignment-5/csv_results/"+"size_"+size+"_threads_" +thread+".csv");
+                    OutputStreamWriter isr = new OutputStreamWriter(fis);
+                    BufferedWriter bw = new BufferedWriter(isr);
+                    int j = 0;
+                    for (long i : timeList) {
+                        String content = (double) (size/10) * (j + 1) / size + "," + (double) i / 10 + "\n";
+                        j++;
+                        bw.write(content);
+                        bw.flush();
+                    }
+                    bw.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            long endTime = System.currentTimeMillis();
-            time = (endTime - startTime);
-            timeList.add(time);
-
-
-            System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
-
         }
-        try {
-            FileOutputStream fis = new FileOutputStream("./src/result.csv");
-            OutputStreamWriter isr = new OutputStreamWriter(fis);
-            BufferedWriter bw = new BufferedWriter(isr);
-            int j = 0;
-            for (long i : timeList) {
-                String content = (double) 10000 * (j + 1) / 2000000 + "," + (double) i / 10 + "\n";
-                j++;
-                bw.write(content);
-                bw.flush();
-            }
-            bw.close();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     private static void processArgs(String[] args) {
